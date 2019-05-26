ee6a706dfb60b59dad9df130c3680979f684f901
YChien <yin_qian@outlook.com>
2019-05-26 16:22:05 +0800

    测试2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App！"/>
+    <HelloWorld msg="Welcome to Your Vue.js App!!!"/>
   </div>
 </template>
 
